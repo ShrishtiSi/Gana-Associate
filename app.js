d6136c829d8581a422dae3324f72e7abd2a29a23
shekharkundra <shekhar.kundra@gmail.com>
2021-12-16 18:22:20 +0530

    email sending html

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ let port = 2300 | process.env.port;
 app.use("/img", express.static(__dirname + "/public/image"));
 app.use("/css", express.static(__dirname + "/public/css"));
 
+app.use("/", require("./routes/email"));
 app.use("/", require("./routes/mainpagesroutes"));
 
 http.createServer(app).listen(port, () => {
--- routes/mainpagesroutes.js
+++ routes/mainpagesroutes.js
@@ -1,7 +1,5 @@
 const express = require("express");
 var router = express.Router();
-let config = require("../config/config.json");
-let transporter = require("../config/mailler");
 
 router.use(express.json());
 router.use(express.urlencoded({ extended: false }));
@@ -26,24 +24,5 @@ router.get("/Contact", (req, res) => {
     res.render("../views/mainpages/contact.ejs");
 });
 
-//route for sending emails
-router.post("/sendemail", async(req, res) => {
-    console.log(req.body);
-    let data = {
-        uname: req.body.uname,
-        uemail: req.body.uemail,
-        uphone: req.body.uphoneno,
-        uloc: req.body.dlf,
-        umsg: req.body.message,
-    };
-    let info = await transporter.sendMail({
-        from: config.uemail, // sender address
-        to: data.uemail, // list of receivers
-        subject: "Hello ✔", // Subject line
-        text: "Hello world?", // plain text body
-        html: "<b>Hello world?</b>", // html body
-    });
-    res.send(data);
-});
 
 module.exports = router;
